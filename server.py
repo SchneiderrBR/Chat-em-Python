8aab0033a57dc61b600edf80242d1373f35b9089
Felipe Schneider <ffelipeschneider@gmail.com>
2020-04-21 17:42:32 -0300

    Alterado para usar Thread

--- server.py
+++ server.py
@@ -1,36 +1,36 @@
 from socket import *
-import select
+from threading import Thread
 
 socketList = []
 users = {}
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('localhost',12000))
+serverSocket.bind(('localhost', 12000))
 serverSocket.listen()
 socketList.append(serverSocket)
 
-while True:
-    ready_to_read,ready_to_write,in_error = select.select(socketList,[],[],0)
-    for sock in ready_to_read:
-        if sock == serverSocket:
-            connect, addr = serverSocket.accept()
-            socketList.append(connect)
-            connect.send(("conectado em: " + str(addr)).encode())
-        else:
-            try:
-                data = sock.recv(2048).decode()
-                if data.startswith("¥"):
-                    users[data[1:].lower()] = connect
-                    print("User " + data[1:] +" added.")
-                    connect.send(("Seu user é: "+str(data[1:])).encode())
-                elif data.find("§@:") != -1:
-                    for u in users:
-                        user = data.split("§")[1]
-                        msg = data.split("§")[2].split(":")[1]
-                        users[u].send((user + ": " + msg).encode())
-                elif data.startswith("§"):
+def client(sock):
+    while True:
+        try:
+            data = sock.recv(2048).decode()
+            if data.startswith("¥"):
+                users[data[1:].lower()] = sock
+                print("User " + data[1:] + " added.")
+                sock.send(("Seu user é: " + str(data[1:])).encode())
+            elif data.find("§@:") != -1:
+                for u in users:
                     user = data.split("§")[1]
                     msg = data.split("§")[2].split(":")[1]
-                    dest = data.split("§")[2].split(":")[0]
-                    users[dest].send((user + ": " + msg).encode())
-            except:
-                continue
+                    users[u].send((user + ": " + msg).encode())
+            elif data.startswith("§"):
+                user = data.split("§")[1]
+                msg = data.split("§")[2].split(":")[1]
+                dest = data.split("§")[2].split(":")[0]
+                users[dest].send((user + ": " + msg).encode())
+        except:
+            continue
+
+while True:
+    sock, addr = serverSocket.accept()
+    socketList.append(sock)
+    sock.send(("conectado em: " + str(addr)).encode())
+    Thread(target=client, args=([sock])).start()
