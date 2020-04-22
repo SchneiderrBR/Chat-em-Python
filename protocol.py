8cd944e0dc38e8f789f848e50cde14e5dbb9b382
Felipe Schneider <ffelipeschneider@gmail.com>
2020-04-22 16:15:48 -0300

    Corrigindo erros do protocolo

--- protocol.py
+++ protocol.py
@@ -1,4 +1,4 @@
-class Protocol:
+class Ptc:
 
     @staticmethod
     def disconnect(message: str) -> bytes:
@@ -6,64 +6,66 @@ class Protocol:
 
     @staticmethod
     def error(message: str) -> bytes:
-        return ('{"op": "ERROR", message: "' + message + '"}').encode()
+        return ('{"op": "ERROR", "message": "' + message + '"}').encode()
 
     @staticmethod
     def login(username: str, hash: str = None) -> bytes:
-        if (hash != None):
+        if hash != None:
             return ('{"op": "LOGIN", "name": "' + username + '", "hash": "' + hash + '"}').encode()
         else:
             return ('{"op": "LOGIN", "name": "' + username + '"}').encode()
 
     @staticmethod
     def message(target: str, mesage: str, username: str = None, hash: str = None) -> bytes:
-        if (hash != None):
+        if hash != None:
             return (
-                        '{"op": "MESSAGE", name: "' + username + '", "target": "' + target + '", "message": "' + mesage + '", hash: "' + hash + '"}').encode()
-        elif (username != None):
+                    '{"op": "MESSAGE", "name": "' + username + '", "target": "' + target + '", "message": "' + mesage + '", hash: "' + hash + '"}').encode()
+        elif username != None:
             return (
-                        '{"op": "MESSAGE", name: "' + username + '", "target": "' + target + '", "message": "' + mesage + '"}').encode()
+                    '{"op": "MESSAGE", "name": "' + username + '", "target": "' + target + '", "message": "' + mesage + '"}').encode()
         else:
             return ('{"op": "MESSAGE", "target": "' + target + '", "message": "' + mesage + '"}').encode()
 
     @staticmethod
     def join(channel: str, username: str, passw: str = None) -> bytes:
-        if (passw != None):
+        if passw != None:
             return (
-                        '{"op": "JOIN", user: "' + username + '", "channel": "' + channel + '", "pass": "' + passw + '"}').encode()
+                    '{"op": "JOIN", "user": "' + username + '", "channel": "' + channel + '", "pass": "' + passw + '"}').encode()
         else:
-            return ('{"op": "JOIN", user: "' + username + '", "channel": "' + channel + '"}').encode()
+            return ('{"op": "JOIN", "user": "' + username + '", "channel": "' + channel + '"}').encode()
 
     @staticmethod
     def part(channel: str, username: str) -> bytes:
-        return ('{"op": "PART", user: "' + username + '", "channel": "' + channel + '"}').encode()
+        return ('{"op": "PART", "user": "' + username + '", "channel": "' + channel + '"}').encode()
 
     @staticmethod
     def admin(channel: str, username: str) -> bytes:
-        return ('{"op": "ADMIN", user: "' + username + '", "channel": "' + channel + '"}').encode()
+        return ('{"op": "ADMIN", "user": "' + username + '", "channel": "' + channel + '"}').encode()
 
     @staticmethod
     def kick(channel: str, username: str, message: str = None) -> bytes:
-        if (message != None):
+        if message != None:
             return (
-                        '{"op": "KICK", user: "' + username + '", "channel": "' + channel + '", message?: "' + message + '"}').encode()
+                    '{"op": "KICK", "user": "' + username + '", "channel": "' + channel + '", message?: "' + message + '"}').encode()
         else:
-            return ('{"op": "KICK", user: "' + username + '", "channel": "' + channel + '"}').encode()
+            return ('{"op": "KICK", "user": "' + username + '", "channel": "' + channel + '"}').encode()
 
     @staticmethod
     def ack(hash: str, success: str) -> bytes:
         return ('{"op": "ACK", "hash": "' + hash + '", "success": ' + success + '}').encode()
 
-    @staticmethod
-    def channels(channelList: str = None) -> bytes:
-        if (channelList != None):
-            return ('{"op": "CHANNELS", channels: ' + channelList + '}').encode()
+    @staticmethod  ###### TEM QUE DAR UMA OLHADA NESSE CARA #####    LISTA DE OBJ? CHAVE VALOR? DUAS LISTAS?
+    def channels(channelList: list = None) -> bytes:
+        if channelList != None:
+            for a in channelList:  ### str = str undefined, funciona?
+                lst = lst + '{"name": "' + a.name + '", "pass": "' + a.passw + '"},'
+            return ('{"op": "CHANNELS", "channels": [' + lst[:-1] + '] }').encode()
         else:
             return '{"op": "CHANNELS"}'.encode()
 
     @staticmethod
     def users(hash: str, channel: str = None) -> bytes:
-        if (channel != None):
+        if channel != None:
             return ('{"op": "USERS", channel?: "' + channel + '", "hash": "' + hash + '"}').encode()
         else:
             return ('{"op": "USERS", "hash": "' + hash + '"}').encode()
