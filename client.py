81557cd325c8de84500fdd630ee5e43f33b3725f
Felipe Schneider <ffelipeschneider@gmail.com>
2020-04-23 17:23:53 -0300

    Comando /Help e /exit

--- client.py
+++ client.py
@@ -17,7 +17,7 @@ else:
 
 rec = clientSocket.recv(1024).decode()
 recvMsg = json.loads(rec)
-print(recvMsg['message'])  # confirmação de conexão
+print(recvMsg['message'])
 
 
 def login():
@@ -45,18 +45,27 @@ def receive():
 login()
 Thread(target=receive).start()
 
-# print("[User:Mensagem]")
 try:
     while True:
         sendMsg = input()
         if sendMsg.startswith("/"):
             if sendMsg.startswith('/join:'):
-                if (len(sendMsg.split(':')) == 2):
+                if len(sendMsg.split(':')) == 2:
                     clientSocket.send(Ptc.join(sendMsg.split(':')[1]))
-                elif (len(sendMsg.split(':')) == 3):
+                elif len(sendMsg.split(':')) == 3:
                     clientSocket.send(Ptc.join(sendMsg.split(':')[1], None, sendMsg.split(':')[2]))
                 else:
-                    print('/help join')
+                    print('/help:join')
+            elif sendMsg.startswith('/exit:'):
+                if len(sendMsg.split(':')) == 2:
+                    clientSocket.send(Ptc.part(sendMsg.split(':')[1]))
+                else:
+                    print('/help:exit')
+            elif sendMsg.startswith('/help:'):
+                if len(sendMsg.split(':')) == 2:
+                    help(sendMsg.split(':')[1])
+                else:
+                    help()
             else:
                 print("commando nao reconhecido, digite /help")
         elif sendMsg.startswith("*:"):
@@ -64,13 +73,40 @@ try:
         elif sendMsg.startswith("&:"):
             clientSocket.send(Ptc.message('&', sendMsg.split(':')[1]))
         elif sendMsg.startswith("#"):
-            if (len(sendMsg.split(':')) == 2):
+            if len(sendMsg.split(':')) == 2:
                 clientSocket.send(Ptc.message(sendMsg.split(':')[0], sendMsg.split(':')[1]))
             else:
-                print('/help message')
+                print('/help:message')
         elif len(sendMsg.split(':')) == 2:
             clientSocket.send(Ptc.message(sendMsg.split(':')[0], sendMsg.split(':')[1]))
         else:
             print("commando nao reconhecido, digite /help")
 except:
     clientSocket.send(Ptc.disconnect())
+
+
+def help(cmd=None):
+    if cmd == "join":
+        print("/join:name         =>  Entra no canal 'name'")
+        print("/join:name:pass    =>  Entra no canal 'name' com a senha 'pass'")
+        print("*Caso o canal nao exista, ele sera criado, e voce sera o admin")
+        print("*Para saber mais sobre admin, digite /help:admin")
+    elif cmd == "exit":
+        print("/exit:name         =>  Sai do canal 'name'")
+    elif cmd == "message":
+        print("user:message       =>  Envia a mensagem 'message' para 'user'")
+        print("#channel:message   =>  Envia a mensagem 'message' para o canal 'channel'")
+        print("&:message          =>  Envia a mensagem 'message' para todos usuarios do servidor")
+        print("*:message          =>  Envia a mensagem 'message' para todos os servidores")
+    elif cmd == "admin":
+        print("/admin:name        =>  Torna 'name' um admin do canal")
+        print("*O primeiro admin sera quem criar o canal")
+        print("*A unica permissao de admin é usar o comando /kick")
+    elif cmd == "kick":
+        print("/kick:name:channel =>  Expulsa 'name' do canal 'channel'")
+        print("*Somente um admin pode usar esse comando")
+    else:
+        print("   - /help:message")
+        print("   - /help:join")
+        print("   - /help:exit")
+        print("   - /help:admin")
