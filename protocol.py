af8501ea70d79c22860b08ee41e317ac1edebd30
Felipe Schneider <ffelipeschneider@gmail.com>
2020-04-23 12:27:32 -0300

    Atualizaçao do Protocolo

--- protocol.py
+++ protocol.py
@@ -1,12 +1,14 @@
 import json
 
-class Ptc:
 
+class Ptc:
     @staticmethod
     def disconnect(message: str = None) -> bytes:
         if message is None:
+            # client-server
             return json.dumps({"op": "DISCONNECT"}).encode()
         else:
+            # server-client
             return json.dumps({"op": "DISCONNECT", "message": message}).encode()
 
     @staticmethod
@@ -16,8 +18,10 @@ class Ptc:
     @staticmethod
     def login(username: str, hash: str = None) -> bytes:
         if hash is None:
+            # client-server
             return json.dumps({"op": "LOGIN", "name": username}).encode()
         else:
+            # server-server
             return json.dumps({"op": "LOGIN", "name": username, "hash": hash}).encode()
 
     # Sends a message, target can be:
@@ -28,28 +32,39 @@ class Ptc:
     @staticmethod
     def message(target: str, message: str, username: str = None, hash: str = None) -> bytes:
         if hash is None and username is None:
+            # client-server
             return json.dumps({"op": "MESSAGE", "target": target, "message": message}).encode()
         elif hash is None:
+            # server-client
             return json.dumps(
                 {"op": "MESSAGE", "name": username, "target": target, "message": message}).encode()
         else:
+            # server-server
             return json.dumps(
                 {"op": "MESSAGE", "name": username, "hash": hash, "target": target, "message": message}).encode()
 
     @staticmethod
     def join(channel: str, username: str = None, passwd: str = None) -> bytes:
         if passwd is None and username is None:
+            # client-server
             return json.dumps({"op": "JOIN", "channel": channel}).encode()
-        elif passwd is None:
+        elif username is None:
+            # client-server
+            return json.dumps({"op": "JOIN", "channel": channel, "pass": passwd}).encode()
+        elif username is not None and passwd is None:
+            # server-server (no password)
             return json.dumps({"op": "JOIN", "channel": channel, "user": username}).encode()
         else:
+            # server-server (with password)
             return json.dumps({"op": "JOIN", "channel": channel, "user": username, "pass": passwd}).encode()
 
     @staticmethod
     def part(channel: str, username: str = None) -> bytes:
         if username is None:
+            # client-server
             return json.dumps({"op": "PART", "channel": channel}).encode()
         else:
+            # server-server
             return json.dumps({"op": "PART", "channel": channel, "user": username}).encode()
 
     @staticmethod
@@ -80,5 +95,7 @@ class Ptc:
             return json.dumps({"op": "USERS", "hash": hash}).encode()
         elif users is None:
             return json.dumps({"op": "USERS", "hash": hash, "channel": channel}).encode()
+        elif channel is None:
+            return json.dumps({"op": "USERS", "hash": hash, "users": users}).encode()
         else:
             return json.dumps({"op": "USERS", "hash": hash, "channel": channel, "users": users}).encode()
